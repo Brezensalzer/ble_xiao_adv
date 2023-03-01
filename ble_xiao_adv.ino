d6e1c11be0db5d5193b59256a42a3361b686ef2c
Stefan Labich <stl@m1mini.modellmarine.de>
2023-03-01 12:48:03 +0100

    send QSPI flash to sleep

--- ble_xiao_adv.ino
+++ ble_xiao_adv.ino
@@ -5,8 +5,17 @@
 #include <bluefruit.h>
 #include <ble_gap.h>
 
+// shutdown flash memory
+#include "Adafruit_SPIFlash.h"
+#include "SdFat.h"
+#include <SPI.h>
+// for flashTransport definition
+#include "flash_config.h"
+
+Adafruit_SPIFlash flash(&flashTransport);
+
 // save power without serial interface...
-#define DEBUG
+//#define DEBUG
 
 struct ATTR_PACKED
 {
@@ -105,7 +114,7 @@ void startAdvertising()
   Bluefruit.Advertising.clearData();
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.setType(BLE_GAP_ADV_TYPE_NONCONNECTABLE_SCANNABLE_UNDIRECTED);
-  Bluefruit.setName("Brezensalzer");
+  Bluefruit.setName("Brezensalzer XIAO");
   Bluefruit.ScanResponse.addName();
   Bluefruit.setTxPower(4);
   
@@ -160,6 +169,12 @@ void setup()
       delay(100);
     }
   }
+
+  // before you put the NRF to sleep put the flash to sleep
+  // this saves about 16 µA
+  flash.begin();
+  flashTransport.runCommand(0xB9);
+  flashTransport.end();
 }
 
 //---------------------------------------------------------------------------
@@ -167,7 +182,7 @@ void loop()
 //---------------------------------------------------------------------------
 {
   
-  // losing 23 µA ???
+  // losing 4.6 µA
   delay(59000);
   startAdvertising();
   delay(500);
